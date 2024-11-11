f85ffcf9ed09cd4dd6ac0fbe238f6d080b4aed83
Richard Stovall <richard.stovall@inboxhealth.com>
2024-11-11 15:26:39 -0500

    ...

--- app/initializers/ember-feature-flags.js
+++ app/initializers/ember-feature-flags.js
@@ -1,16 +1,16 @@
-import config from '../config/environment';
-import Features from '../features/-main';
+import config from "../config/environment";
+import Features from "../features/-main";
 
-export function initialize( registry, application ) {
-  var serviceName = config.featureFlagsService || 'features';
-  application.register('features:-main', Features);
-  application.inject('route', serviceName, 'features:-main');
-  application.inject('controller', serviceName, 'features:-main');
-  application.inject('component', serviceName, 'features:-main');
-  application.inject('features:-main', 'application', 'application:main');
+export function initialize(application) {
+  var serviceName = config.featureFlagsService || "features";
+  application.register("features:-main", Features);
+  application.inject("route", serviceName, "features:-main");
+  application.inject("controller", serviceName, "features:-main");
+  application.inject("component", serviceName, "features:-main");
+  application.inject("features:-main", "application", "application:main");
 }
 
 export default {
-  name: 'ember-feature-flags',
-  initialize: initialize
+  name: "ember-feature-flags",
+  initialize: initialize,
 };
